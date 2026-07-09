2061f95df8c67930a974aaecf52fb6c8fe46405c
IsaacGM00 <A212303A@correo.uia.mx>
2026-07-09 13:30:58 -0600

    Corrección de código duplicado

--- src/main/java/com/minsait/apirest/api_rest/model/User.java
+++ src/main/java/com/minsait/apirest/api_rest/model/User.java
@@ -1,9 +1,13 @@
 package com.minsait.apirest.api_rest.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,36 +15,4 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
